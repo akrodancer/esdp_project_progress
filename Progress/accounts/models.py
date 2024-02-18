e0b05a8875f219ff617ce292d12f8669f313b712
Selfierro <selfierro@mail.ru>
2024-02-18 15:40:51 -0500

    Модель пользователя

--- Progress/accounts/models.py
+++ Progress/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    ROLE_CHOICES = (
+        ('teacher', 'Teacher'),
+        ('user', 'User'),
+    )
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return self.username
