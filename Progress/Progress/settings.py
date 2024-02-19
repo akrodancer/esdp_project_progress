07ba51acfe5a4eed38b9d29fb5c9fcb8ce3be126
Selfierro <selfierro@mail.ru>
2024-02-18 21:48:08 -0500

    добавил миграции и добавил приложения в настройках

--- Progress/Progress/settings.py
+++ Progress/Progress/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
+    'tests',
+    'courses',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
