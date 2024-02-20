548709cb6dbb54e7e3aa48490a5ab1ba5957ab78
Selfierro <selfierro@mail.ru>
2024-02-20 15:32:13 -0500

    Фикс конфликта миграций, изменена логика проверки оплаты студентом

--- Progress/accounts/models.py
+++ Progress/accounts/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from tests.models import Test
 
 
 class User(AbstractUser):
+    avatar = models.ImageField(upload_to='...', null=True, blank=True)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
     ROLE_CHOICES = (
@@ -11,7 +11,6 @@ class User(AbstractUser):
         ('user', 'User'),
     )
     role = models.CharField(max_length=30, choices=ROLE_CHOICES)
-    paid_tests = models.ManyToManyField(Test, blank=True)
 
     def __str__(self):
         return self.username
