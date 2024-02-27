3cfc4216cefd8e3cdc4bf846ae700cb8e3dc3cee
TalkingCoffeePot <barrberry666@gmail.com>
2024-02-28 01:58:52 +0500

    Ошибка контекстной авторизации
    	modified:   accounts/context_processor.py
    
        Исправлена ошибка:
        - При попытке регистрации уже существующего профиля не возникает ошибки обработчика

--- source/accounts/context_processor.py
+++ source/accounts/context_processor.py
@@ -3,16 +3,10 @@ from accounts.forms import LoginUserForm
 
 def log_in_processor(request):
     if request.method == 'GET':
-        form_log = LoginUserForm()
-    elif request.method == 'POST':
-        form_log = LoginUserForm(request.POST)
-        if form_log.is_valid():
-            pass
-    else:
-        form_log = None
-        data = {
-            'form_log': form_log,
-        }
-        return data
-    if request.method == 'POST':
-        return {}
+            form_log = LoginUserForm(request.POST or None)
+
+            data = {
+                'form_log': form_log,
+            }
+            return data
+    return {}
