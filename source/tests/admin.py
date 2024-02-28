d24e754817ea04414546a7075eedb0c9bfcc17f1
Selfierro <selfierro@mail.ru>
2024-02-28 04:32:10 -0500

    Рефакторинг административных представлений и обновление моделей Course и Test Коммит включает в себя рефакторинг административных представлений в приложениях courses и tests. К моделям Course и Test были добавлены декораторы администратора, позволяющие просмотр списка с определенными столбцами. Кроме того, были применены изменения структуры кода в начальном скрипте миграции теста. Была обновлена дата создания скрипта миграции и были переорганизованы зависимости модели, что улучшило общую поддерживаемость скрипта.

--- source/tests/admin.py
+++ source/tests/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Test, Question, Answer, UserTest, UserAnswer
 
-# Register your models here.
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'test_name']
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question_name', 'question_text']
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'is_correct', 'answer_text']
+
+@admin.register(UserTest)
+class UserTestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'test', 'date_taken']
+
+@admin.register(UserAnswer)
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_test', 'question', 'answer']
