6e158b8b356675e0129d3e694c92d5da857e18ef
l0st666 <TalkingCoffeePot>
2024-04-08 00:53:14 +0500

    #11 Выполнен коллектстатик, добавлен конфиг nginx

--- source/main_config/settings.py
+++ source/main_config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'localhost', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'localhost', 'localhost', '127.0.0.1', 'progress']
 
 
 # Application definition
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = join(BASE_DIR)
+STATIC_ROOT = join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
---
+++ source/static/js/modal-sign.js
@@ -0,0 +1,48 @@
+ //-------------------ЗАКРЫТИЕ МОДАЛЬНЫХ ОКОН----------------------//
+ $("#back").on('click', function(e) {
+     $('body').css('overflow', 'scroll');
+     e.preventDefault();
+     $(".turn-on").prop('hidden', 'true')
+     $(".sign-up").prop('hidden', 'true')
+ });
+ $("#close").on('click', function(e) {
+    $(".confirm").prop('hidden', 'true')
+    $(".form-cover").prop('hidden', 'true')
+});
+
+ //-------------------МОДАЛЬНОЕ ОКНО ЛОГИНА----------------------//
+ $("#sign_in_button").on('click', function(e) {
+     $('body').css('overflow', 'hidden')
+     e.preventDefault();
+     $(".turn-on").removeAttr('hidden')
+ });
+
+  //-------------------МОДАЛЬНОЕ ОКНО ЗАЯВКИ----------------------//
+ $("#sign_up_button").on('click', function(e) {
+    $('body').css('overflow', 'hidden')
+    e.preventDefault();
+    $(".sign-up").removeAttr('hidden')
+});
+
+ //-------------------МЕТОД ОТПРАВКИ ЗАПРОСА----------------------//
+$('#course_sign').on('click', function(e) {
+    let rawData = {
+        first_name: $('#first_name').val(),
+        last_name: $('#last_name').val(),
+        phone: $('#phone').val(),
+        email: $('#email').val(),
+        course: $('#course').val(),
+    };
+    $.ajax({
+        headers: {"X-CSRFToken": document.querySelector('[name=csrfmiddlewaretoken]').value},
+        type: 'POST',
+        url: 'accounts/sign_up/',
+        contentType: 'application/json',
+        data: JSON.stringify(rawData),
+        success: function(json) {
+            $(".form-cover").removeAttr('hidden')
+            $(".confirm").removeAttr('hidden')
+        }
+    });
+    e.preventDefault();
+})
