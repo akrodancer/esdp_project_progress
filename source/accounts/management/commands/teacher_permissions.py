bb2c1dd43ea88fcdfe0f4e6cfbe6e3a352e89eb4
gavril <gokhlopkov2759@gmail.com>
2024-04-26 21:10:19 +0500

    Добавил некоторые разрешения

--- source/accounts/management/commands/teacher_permissions.py
+++ source/accounts/management/commands/teacher_permissions.py
@@ -2,7 +2,8 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from accounts.models import User, Comment
-from courses.models import Visit, Lesson
+from courses.models import Visit, Lesson, Course, Group, LessonPerGroup
+from online_tests.models import OnlineTest, Question
 
 
 class Command(BaseCommand):
@@ -24,29 +25,57 @@ class Command(BaseCommand):
 
         teachers = User.objects.filter(role='teacher')
 
-        comment_permissions = {
-            'add_comment': 'Can add comment',
-            'change_comment': 'Can change comment',
-            'delete_comment': 'Can delete comment',
-            'view_comment': 'Can view comment',
-        }
-        assign_permissions_for_model(Comment, comment_permissions)
+        models_to_manage = [Course, OnlineTest, Lesson, Visit, Question, OnlineTest, Group, LessonPerGroup]
 
-        visit_permissions = {
-            'add_visit': 'Can add visit',
-            'change_visit': 'Can change visit',
-            'delete_visit': 'Can delete visit',
-            'view_visit': 'Can view visit',
+        permissions_mapping = {
+            Course: {
+                'add_course': 'Can add course',
+                'change_course': 'Can change course',
+                'delete_course': 'Can delete course',
+                'view_course': 'Can view course',
+            },
+            Lesson: {
+                'add_lesson': 'Can add lesson',
+                'change_lesson': 'Can change lesson',
+                'delete_lesson': 'Can delete lesson',
+                'view_lesson': 'Can view lesson',
+            },
+            Visit: {
+                'add_visit': 'Can add visit',
+                'change_visit': 'Can change visit',
+                'delete_visit': 'Can delete visit',
+                'view_visit': 'Can view visit',
+            },
+            Question: {
+                'add_question': 'Can add question',
+                'change_question': 'Can change question',
+                'delete_question': 'Can delete question',
+                'view_question': 'Can view question',
+            },
+            OnlineTest: {
+                'add_onlinetest': 'Can add online test',
+                'change_onlinetest': 'Can change online test',
+                'delete_onlinetest': 'Can delete online test',
+                'view_onlinetest': 'Can view online test',
+            },
+            Group: {
+                'add_group': 'Can add group',
+                'change_group': 'Can change group',
+                'delete_group': 'Can delete group',
+                'view_group': 'Can view group',
+            },
+            LessonPerGroup: {
+                'add_lessonpergroup': 'Can add lesson per group',
+                'change_lessonpergroup': 'Can change lesson per group',
+                'delete_lessonpergroup': 'Can delete lesson per group',
+                'view_lessonpergroup': 'Can view lesson per group',
+            }
         }
-        assign_permissions_for_model(Visit, visit_permissions)
 
-        lesson_permissions = {
-            'add_lesson': 'Can add lesson',
-            'change_lesson': 'Can change lesson',
-            'delete_lesson': 'Can delete lesson',
-            'view_lesson': 'Can view lesson',
-        }
-        assign_permissions_for_model(Lesson, lesson_permissions)
+        # Применение разрешений для каждой модели
+        for model_class in models_to_manage:
+            if model_class in permissions_mapping:
+                assign_permissions_for_model(model_class, permissions_mapping[model_class])
 
         self.stdout.write(self.style.SUCCESS(
             f"Разрешения выданы"
